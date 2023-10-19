d87ba44f69b02b9149499999ddc4c400b5fbb920
LouAnneSVTR <louanne.sauvetre@gmail.com>
2023-10-19 10:00:13 +0000

    python arengment

--- lddm_svtr.py
+++ lddm_svtr.py
@@ -4,7 +4,6 @@ import sys
 import google.cloud.dataproc_v1
 from google.cloud import dataproc_v1 as dataproc
 
-
 def create_cluster(project_id, region, cluster_name):
     # Create the cluster client.
     cluster_client = dataproc.ClusterControllerClient(
@@ -28,31 +27,6 @@ def create_cluster(project_id, region, cluster_name):
 
     print(f"Cluster created successfully: {result.cluster_name}")
 
-    ############# JOB #############    
-    # Create the job client for Pig
-    job_client = dataproc.JobControllerClient(
-        client_options={"api_endpoint": f"{region}-dataproc.googleapis.com:443"}
-    )
-
-    # Create the job config.
-    job = {
-        "placement": {"cluster_name": cluster_name},
-        "pig_job": {"main_python_file_uri": f"gs://lsdm_data_svtr/dataproc.py"},
-    }
-
-    operation = job_client.submit_job_as_operation(
-        request={"project_id": project_id, "region": region, "job": job}
-    )
-    #response = operation.result()
-
-    print(f"Pig Job finished successfully: ")
-    ############# JOB #############
-    
-
-    #gsutil rm -rf gs://lsdm_data_svtr/out
-    #gcloud dataproc jobs submit pig --region europe-west1 --cluster cluster-a35a -f gs://lsdm_data_svtr/dataproc.py
-
-
     # Delete the cluster once the job has terminated.
     operation = cluster_client.delete_cluster(
         request={
@@ -66,17 +40,46 @@ def create_cluster(project_id, region, cluster_name):
     print(f"Cluster {cluster_name} successfully deleted.")
 
 
- 
+
+#def clean_data(project_id, region, cluster_name):
+    ## copy data
+    #os.system("gsutil cp small_page_links.nt gs://myown_bucket/")
+    ## copy pig code
+    #os.system("gsutil cp pagerank-notype.py gs://myown_bucket/")
+    ## Clean out directory
+    #os.system("gsutil rm -rf gs://myown_bucket/out")
+
+
+
+def run_pig_job(project_id, region, cluster_name):
+
+    #os.system("gcloud dataproc jobs submit pig --region europe-west1 --cluster cluster-a35a -f gs://lsdm_data_svtr/dataproc.py")
+    print(f"Job finished successfully: ")
+
+
+def run_spark_job(project_id, region, cluster_name):
+
+    #os.system("gcloud dataproc jobs submit pig --region europe-west1 --cluster cluster-a35a -f gs://lsdm_data_svtr/dataproc.py")
+    print(f"Job finished successfully: ")
+
+
+
+#def collect_data(project_id, region, cluster_name):
+
+
+
 if __name__ == "__main__":
     if len(sys.argv) < 4:
-        sys.exit("python create_cluster.py project_id region cluster_name")
-
-    project_id = sys.argv[1]
-    region = sys.argv[2]
-    cluster_name = sys.argv[3]
+        project_id = "lsdm-40181"
+        region = "europe-central2"
+        cluster_name = "clusterlsdm"
+    else:
+        project_id = sys.argv[1]
+        region = sys.argv[2]
+        cluster_name = sys.argv[3]
 
     create_cluster(project_id, region, cluster_name)
 
-
+#lsdm-401811 europe-central2 test
 # how to run PySpark job
 #https://www.freecodecamp.org/news/what-is-google-dataproc/
